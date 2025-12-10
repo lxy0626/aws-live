3a85db9f08548928ff38a42062c5694e7ee2f69b
Xian Yu Lai <130376274+lxy0626@users.noreply.github.com>
2025-12-10 16:48:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cpce8iymuze7.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "laixianyu-employee"
 customregion = "us-east-1"
 
